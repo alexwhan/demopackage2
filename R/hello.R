87a74b5627460d72b8a4b93218940ee4d9ebd4ce
Alex Whan <wha022@csiro.au>
2025-02-20 15:51:20 +1100

    added a function and a test

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,15 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   https://r-pkgs.org
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' Add some numbers
+#'
+#' @param x The first number
+#' @param y The second number
+#'
+#' @return The sum
+#' @export
+#'
+#' @examples
+#'
+#' add_number(5, 2)
+#'
+add_number <- function(x, y) {
+  sum(x, y)
 }
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,3 @@
+test_that("The add function works", {
+  expect_equal(add_numbers(5, 2), 7)
+})
